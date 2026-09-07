8d630cbfc75af3d91611bad4b5793a483f9b0db9
UceroDev <annaucero2@gmail.com>
2026-09-07 10:07:00 +0200

    add links to music videos in a ts file

---
+++ src/data/music-videos.ts
@@ -0,0 +1,22 @@
+export const musicvideos = [
+  {
+    name: "Pájaros Verdes",
+    link: "https://youtu.be/k5-GL-Rdhxw",
+  },
+  {
+    name: "Cartuchos de Cristal",
+    link: "https://youtu.be/zBxSNMCeTwU",
+  },
+  {
+    name: "Hasta Que Pare El Viento",
+    link: "https://youtu.be/aWsxvgAqTfU",
+  },
+  {
+    name: "Octubre",
+    link: "https://youtu.be/M45l-rUEsfw",
+  },
+  {
+    name: "Momentos",
+    link: "https://youtu.be/ruHK2owst24",
+  },
+];
