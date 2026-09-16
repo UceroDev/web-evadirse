9a26c12124bf068bd46968164d345dbb0ceae566
UceroDev <annaucero2@gmail.com>
2026-09-16 12:51:01 +0200

    add cloudflare adapter to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
+  adapter: cloudflare(),
   integrations: [],
   vite: {
     plugins: [tailwindcss()]
